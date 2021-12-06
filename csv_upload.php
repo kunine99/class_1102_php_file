8e2ee775e764eb4aa4748f5acac985242291b6dc
rong <rongyy94job@gmail.com>
2021-12-06 09:49:52 +0800

    建立csv檔案

---
+++ csv_upload.php
@@ -0,0 +1,32 @@
+<?php
+//$_FILES['file']['error']==0
+if(!empty($_FILES['file']['tmp_name'])){
+    $filename=md5(time());
+    // $subname=$_FILES['file']['name'];
+    // $subname=explode(".",$subname); */
+
+
+    $subname=explode(".",$_FILES['file']['name'])[1];
+
+    $newFileName=$filename.".".$subname;
+
+    echo "new=>".$newFileName."<br>";
+    echo "tmp_name=>".$_FILES['csv']['tmp_name']."<br>";
+    echo "fileOrignName=>".$_FILES['csv']['name']."<br>";
+    
+    move_uploaded_file($_FILES['csv']['tmp_name'],"file/".$newFileName);
+
+
+    echo "<a href='file/{$newFileName}'>>"
+
+
+}
+
+
+
+?>
+
+
+
+
+?>
--- file_upload.php
+++ file_upload.php
@@ -1,18 +1,22 @@
 <?php
 //$_FILES['file']['error']==0
-if(!empty($_FILES)['file']['tmp_name'])){
+if(!empty($_FILES['file']['tmp_name'])){
     // md5亂碼產生器
     $filename=md5(time());
     // 我先把你的檔案名稱抓出來(不是tmp_name是name!!)
-    $subname=$_FILES['file']['name'];
+    // $subname=$_FILES['file']['name'];
+    // $subname=explode(".",$subname); */
+
     // explode回傳的會是一個陣列
     $subname=explode(".",$_FILES['file']['name'])[1];
 
     $newFileName=$filename.".".$subname;
 
-    echo "new=>".$newFileName;
-    echo "tmp_name=>".$_FILES['file']['name']."<br>";
-    echo "tmp_name=>".$_FILES['file']['name']."<br>";
+    echo "new=>".$newFileName."<br>";
+    echo "tmp_name=>".$_FILES['file']['tmp_name']."<br>";
+    echo "fileOrignName=>".$_FILES['file']['name']."<br>";
+    
+    move_uploaded_file($_FILES['file']['tmp_name'],"file/".$newFileName);
 
 // 從原本tmp_name的路徑把它搬到我這個目錄下，去加上newfilename去產生新的名稱
 
